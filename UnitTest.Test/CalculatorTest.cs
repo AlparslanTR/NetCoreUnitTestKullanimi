063c1c483e6bb49783c9bbc80fb0af28acc836ae
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-01-01 18:43:45 +0300

    Assert metotları işlendi.

--- UnitTest.Test/CalculatorTest.cs
+++ UnitTest.Test/CalculatorTest.cs
@@ -15,20 +15,47 @@ namespace UnitTest.Test
         {
             // Arrange -- Değişkenleri Verdiğimiz Yerdir.
 
-            int a = 10;
-            int b = 20;
-            var calculator = new Calculator();
+            //int a = 10;
+            //int b = 20;
+            //var calculator = new Calculator();
 
             // Act -- Test Edilecek Metotların Çalıştığı Yerdir
 
-            var total=calculator.Add(a, b);
+            //var total=calculator.Add(a, b);
 
             // Assert -- Doğrulama Evresidir. Çıkan Sonucun Doğru olup olmadığını Test ettiğimiz yerdir.
 
-            Assert.Equal(30,total); // İki değer aynı ise test başarılı geçer.
+            /* Assert.Equal(30,total);*/ // İki değer aynı ise test başarılı geçer.
             /*Assert.NotEqual(30, total);*/ // İki değerden biri farklı olursa test başarılı geçer ama burada ikiside aynı olacağı için başarısız olacak.
 
-        }
+            /*  ////////////////////////////  */
 
+            /*Assert.Contains("Kayhan", "Alparslan Akbaş");*/ // İçinde Kayhan kelimesini bulamadığı için hata verecek.
+            /* Assert.DoesNotContain("Kayhan", "Alparslan Akbaş");*/ // İçinde kayhan kelimesi olmayacak dediğimiz için bu sefer doğru çalışıcak.
+
+            /*  ////////////////////////////  */
+
+            //var names = new List<string>() { "Alparslan", "Kayhan", "Ali", };
+            //Assert.Contains(names, x => x == "Gazi");
+
+            /*  ////////////////////////////  */
+
+            //var regex = "^dog"; 
+            //Assert.Matches(regex, "dog alparslan"); // Test sürecinde gelen değerin bildirilmiş olan Regex ifadesine uyup uymadığını kontrol eden metotlardır.
+
+            /*  ////////////////////////////  */
+
+            /* Assert.StartsWith("Alparslan", "Alparslan Akbas");*/  // Başta girilen string değerin istenilen değer en başta olup olmamasını kontrol eder.
+            /* Assert.EndsWith("Alparslan", "Akbaş Alparslan");*/   // Başta girilen string değerin istenilen değer en sonda olup olmamasını kontrol eder.
+
+            /*  ////////////////////////////  */
+
+            /* Assert.IsType<string>("Alparslan");*/ // Belirlenen tipi kontrol eder.
+            /*Assert.IsNotType<string>(41);*/ // Belirlenen Tip doğru değilse kontrol eder.
+
+            /*  ////////////////////////////  */
+
+
+        }
     }
 }
