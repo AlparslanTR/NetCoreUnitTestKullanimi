3e3b7e824e55c261556df3e17a82a9233fdb4adc
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-01-11 16:39:17 +0300

    .Net Core Mvc için ufak bir crud işlemli proje hazırlandı.

---
+++ ProductMvc/Data/ProductMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductMvc.Models;
+
+namespace ProductMvc.Data
+{
+    public class ProductMvcContext : DbContext
+    {
+        public ProductMvcContext (DbContextOptions<ProductMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductMvc.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ ProductMvc/Repository/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMvc.Models;
+
+namespace ProductMvc.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly MvcUnitTestContext _comtext;
+        private readonly DbSet<TEntity> _dbSet;
+        public Repository(MvcUnitTestContext comtext)
+        {
+            _comtext = comtext;
+            _dbSet = _comtext.Set<TEntity>();
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _comtext.SaveChangesAsync();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _comtext.SaveChanges();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _comtext.Update(entity).State=EntityState.Modified;
+            _dbSet.Update(entity);
+            _comtext.SaveChanges();
+        }
+
+    }
+}
